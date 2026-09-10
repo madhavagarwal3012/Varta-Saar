ddd3fa135b371414de42dde0f2eb6d0bea10d4d8
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2026-09-10 20:01:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,6 @@ from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, HRFlowable
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib import colors
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFonts
 
 # =========================================================================
 # === STEP 1: CONFIGURATION AND API CLIENT SETUP (Backend Handling) =======
